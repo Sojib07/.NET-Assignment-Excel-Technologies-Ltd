c3b6dd72e61a963252bb4410d7934932b7fb0e3c
sojib <Sojibzaman0715@gmail.com>
2023-02-11 13:07:35 +0600

    Migration applied for entities in Database

---
+++ CustomerInformation/CustomerInformation.Infrastructure/Seeds/CountrySeed.cs
@@ -0,0 +1,27 @@
+﻿using CustomerInformation.Infrastructure.Entities;
+
+namespace CustomerInformation.Infrastructure.Seeds
+{
+    public class CountrySeed
+    {
+        public Worker[] Workers
+        {
+            get
+            {
+                return new Worker[]
+                {
+                    new Worker{PostName="Stenographer-Cum-Computer Operator",Id=11000001,User="11LQWESM",Name="MD ISRAFIL SORKER",FathersName="ABDUL BATEN SARKER",MothersName="SALINA AKTER",DateOfBirth=new DateTime(1992,2,22),PermanentDistrict="Munshiganj",Quota="Ansar-VDP" },
+                    new Worker{ PostName="Stenographer-Cum-Computer Operator",Id=11000002,User="146DK6YZ",Name="SYFUL ISLAM",FathersName="MD. ABDUL GONI",MothersName="HASINA AKTHER",DateOfBirth=new DateTime(1993,2,10),PermanentDistrict="Kishorganj",Quota="Physically Handicapped"  },
+                    new Worker{ PostName="Stenographer-Cum-Computer Operator",Id=11000003,User="16ZHEPC4",Name="SUJAN",FathersName="MD ALIMUDDIN MOLLA",MothersName="SEFALI BEGUM",DateOfBirth=new DateTime(1996,10,6),PermanentDistrict="Rajbari",Quota="Non Quota"  },
+                    new Worker{ PostName="Stenographer-Cum-Computer Operator ",Id=11000004,User="1718Q6MA",Name="SUBARNA AKHTER",FathersName="MD. ENAMUL HAQUE KHAN",MothersName="RINA BEGUM",DateOfBirth=new DateTime(1994,6,8),PermanentDistrict="Gopalganj",Quota="Non Quota" },
+                    new Worker{  PostName="Stenographer-Cum-Computer Operator ",Id=11000005,User="17NUT467",Name="LUTFOR RAHMAN",FathersName="LIAKOT ALI",MothersName="RASIDA BEGUM",DateOfBirth=new DateTime(1991,7,29),PermanentDistrict=" Rajbari",Quota="Ansar-VDP"  },
+                    new Worker{ PostName="Stenographer-Cum-Computer Operator ",Id=11000006,User="1BATJVHD",Name="MOST. MAHBUBA KHANOM",FathersName="MD. NAOSHADUL HAQUE",MothersName="MOST. KHALEDA BIBI",DateOfBirth=new DateTime(1991,7,10),PermanentDistrict="Naogaon",Quota="Non Quota"  },
+                    new Worker{ PostName="Stenographer-Cum-Computer Operator ",Id=11000007,User="1F26E9FU",Name="KRISHNO KUMAR SARKAR",FathersName="NARAYAN CHANDRA SARKAR",MothersName="NANDA RANI SARKAR",DateOfBirth=new DateTime(1992,12,30),PermanentDistrict="Bogura",Quota="Non Quota" },
+                    new Worker{ PostName="Stenographer-Cum-Computer Operator ",Id=11000008,User="1EV6P37Z",Name="PROSENJIT KUMAR DAS",FathersName="NARAYAN KUMAR DAS",MothersName="BASONA RANI DAS",DateOfBirth=new DateTime(1992,8,28),PermanentDistrict="Khulna",Quota="Non Quota"  },
+                    new Worker{ PostName="Stenographer-Cum-Computer Operator ",Id=11000009,User="11LQWESM",Name="MD. TAREK AZIZ",FathersName="MD. MOYEN UDDIN",MothersName="SHUKHZAN KHATUN",DateOfBirth=new DateTime(1990,9,21),PermanentDistrict="Kushtia",Quota="Child of Freedom Fighter"  },
+                    new Worker{ PostName="Stenographer-Cum-Computer Operator ",Id=11000010,User="1JXCS1TV",Name="ANOUP KUNDO",FathersName="MONORANJAN KUNDO",MothersName="ANJANA KUNDO",DateOfBirth=new DateTime(1990,11,20),PermanentDistrict="Manikganj",Quota="Non Quota" }
+                };
+            }
+        }
+    }
+}
---
+++ CustomerInformation/CustomerInformation.Web/Data/Migrations/20230211070105_AddTablesInDatabase.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CustomerInformation.Web.Data.Migrations
+{
+    public partial class AddTablesInDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CountryName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    FatherName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    MotherName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    MaritalStatus = table.Column<int>(type: "int", nullable: false),
+                    CustomerPhoto = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    CountryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Customers_Countries_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Countries",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomerAddresses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Address = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    CustomerID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerAddresses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CustomerAddresses_Customers_CustomerID",
+                        column: x => x.CustomerID,
+                        principalTable: "Customers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerAddresses_CustomerID",
+                table: "CustomerAddresses",
+                column: "CustomerID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_CountryId",
+                table: "Customers",
+                column: "CountryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomerAddresses");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Countries");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
